b21526eaf8937540b05cee49de105da8d4a8bb1a
Cleyvison Matos <cleyvisonmatos777@gmail.com>
2020-06-22 17:10:05 -0300

    front end implementado ClassTamanho 22/06

--- src/app/cadastro-classtamanhoindividuo/cadastro-classtamanhoindividuo.component.ts
+++ src/app/cadastro-classtamanhoindividuo/cadastro-classtamanhoindividuo.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { LazyLoadEvent } from './../../primeng/components/common/lazyloadevent.d';
+import { CadClassTamanhoIndividuo } from './../core/model';
+import { ClasseTamanhoIndividoService, ClassIndividuoFiltro } from './classe-tamanho-individo.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ConfirmationService } from 'primeng/primeng';
+import { ToastyService } from 'ng2-toasty';
+import { ErrorHandlerService } from './../core/error-handler.service';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-cadastro-classtamanhoindividuo',
@@ -6,10 +13,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cadastro-classtamanhoindividuo.component.css']
 })
 export class CadastroClasstamanhoindividuoComponent implements OnInit {
+  totalregistrosClassTamanho = 0;
+  listaClasseTamanho = [];
+  cadClassTamanhoIndividuo = new CadClassTamanhoIndividuo();
+  filtro = new ClassIndividuoFiltro();
+  @ViewChild('tabela') grid;
 
-  constructor() { }
+  constructor(
+    private classeTamanhoIndividoService: ClasseTamanhoIndividoService,
+    private errorHandler: ErrorHandlerService,
+    private toasty: ToastyService,
+    private confirmation: ConfirmationService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
 
   ngOnInit() {
   }
 
+  consultaClasseTamanho(page = 0){
+    this.filtro.page = page;
+    this.classeTamanhoIndividoService.consultar(this.filtro)
+     .then(resultado =>{
+       this.totalregistrosClassTamanho = resultado.total;
+       this.listaClasseTamanho = resultado.listaClasseTamanho;
+     })
+     .catch(erro => this.errorHandler.handle(erro));
+  }
+  aoMudarPaginaClasseTamanho(event: LazyLoadEvent) {
+    const page = event.first / event.rows;
+    this.consultaClasseTamanho(page);
+  }
 }
--- src/app/cadastro-classtamanhoindividuo/classe-tamanho-individo.service.ts
+++ src/app/cadastro-classtamanhoindividuo/classe-tamanho-individo.service.ts
@@ -1,9 +1,40 @@
+import { Http, Headers } from '@angular/http';
 import { Injectable } from '@angular/core';
 
+
+export class ClassIndividuoFiltro {
+  page = 0;
+  size = 15;
+  nmClasseTamanho: string;
+}
 @Injectable({
   providedIn: 'root'
 })
 export class ClasseTamanhoIndividoService {
 
-  constructor() { }
-}
+ ClassIndividuoURL = 'http://localhost:8082/classtamanhoindividuo';
+
+  constructor( private http: Http) { }
+
+         //consultar 
+  consultar(filtro: ClassIndividuoFiltro): Promise<any> {
+    const params = new URLSearchParams;
+        const headers = new Headers;
+        params.set('page', filtro.page.toString());
+        params.set('size', filtro.size.toString());
+         if(filtro.nmClasseTamanho){
+           params.set('nmClasseTamanho', filtro.nmClasseTamanho);
+         }
+         return this.http.get(`${this.ClassIndividuoURL}`, { headers, search: filtro})
+         .toPromise()
+         .then(response => {
+          const responseJson = response.json();
+          const listaClasseTamanho = responseJson.content;
+          const resultado = {
+            listaClasseTamanho,
+            total: responseJson.totalElements
+          };
+          return resultado;
+         });
+      }
+  }
